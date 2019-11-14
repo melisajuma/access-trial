7c5d178ced2a6e050c260307fe5d6173da130add
melisajuma <Melisaakinyi95@gmail.com>
2019-11-14 15:10:27 +0300

    chnages to server configuration format

--- moringa/wsgi.py
+++ moringa/wsgi.py
@@ -9,8 +9,7 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
+from django.core.handlers.wsgi import WSGIHandler
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'moringa.settings')
-
-application = get_wsgi_application()
+os.environ['DJANGO_SETTINGS_MODULE'] = 'myapp.settings'
+application = WSGIHandler()
