2cf6bddd4dda8f65e2652294c0e56001c9f301ea
melisajuma <Melisaakinyi95@gmail.com>
2019-11-14 15:07:52 +0300

    creating view functions

--- access/views.py
+++ access/views.py
@@ -1,3 +1,45 @@
+from django.shortcuts import render, redirect
+from .models import *
+from django.http import Http404
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from .sheet2 import form_responses, process_response
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from .filters import UserFilter
 
 # Create your views here.
+#class Profileview(viewsets.ModelViewSet):
+    #queryset= Profile.objects.all()
+    #serializer_class = ProfileSerializer
+def homepage(request):
+    '''
+    assuming we make the api call
+    
+    '''
+    # form_data=form_responses()
+    # form_data=form_responses()
+    response = process_response()
+
+    for email in  interestModel.objects.values_list('email', flat=True).distinct():
+        interestModel.objects.filter(pk__in= interestModel.objects.filter(email=email).values_list('id', flat=True)[1:]).delete()
+
+    res= interestModel.objects.all()
+    return render(request,'index.html',{'data':res})
+
+def scorecard(request):
+    '''
+    Assuming we make the api call
+        
+    '''
+    # form_data=form_responses()
+    # form_data=form_responses()
+    response = process_response()
+    res= scoreModel.objects.all()
+    return render(request,'scores.html.html',{'data':res})
+    
+
+def search(request):
+    user_list = User.objects.all()
+    user_filter = UserFilter(request.GET, queryset=user_list)
+    return render(request, 'search/user_list.html', {'filter': user_filter})
