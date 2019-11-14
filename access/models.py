3a49f8e94b5155cd584b51f899e0c8e9695f981b
melisajuma <Melisaakinyi95@gmail.com>
2019-11-14 15:06:46 +0300

    Adding models

--- access/models.py
+++ access/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+#from .sheet2 import form_responses, process_response
 
-# Create your models here.
+class interestModel(models.Model):
+    
+    email=models.CharField(max_length=250,default='Nan')
+    name=models.CharField(max_length=50)
+    phone_number=models.CharField(max_length=250)
+    guardians_number=models.TextField(max_length=250,default='Nan')
+    age=models.TextField(max_length=250)
+    date_of_birth=models.CharField(max_length=250)
+    gender=models.TextField(max_length=250)
+    prior_acceptance=models.TextField(max_length=250)
+    highest_education_level=models.TextField(default='Nan',max_length=250)
+    bachelors_degree=models.CharField(max_length=250,default='Nan' )
+    completion_date=models.CharField(max_length=250)
+    applied_to_uni=models.CharField(max_length=250)
+    start_date=models.CharField(max_length=250)
+    moringa_student=models.CharField(max_length=250)
+    class_name=models.CharField(max_length=250)
+    commitment=models.CharField(max_length=250)
+    refered_by=models.CharField(max_length=250) 
+    computer_literacy=models.CharField(max_length=250)   
+    fluency=models.CharField(max_length=250)   
+    residence=models.CharField(max_length=250)   
+    residence_other=models.CharField(max_length=250)   
+    residence_clarification=models.CharField(max_length=250)     
+
+class scoreModel(models.Model):
+    name=models.CharField(max_length=250)
+    email=models.EmailField(max_length=250)
+    number=models.IntegerField(max_length=250)
+    score=models.IntegerField(max_length=250)
+    assesment_time=models.CharField(max_length=250)
